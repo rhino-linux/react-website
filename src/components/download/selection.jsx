bdf437434776fbe459d186e43f3b165020644443
ook37 <oren+12345@taumoda.com>
2024-09-21 18:12:15 -0400

    fix download banner

--- src/components/download/selection.jsx
+++ src/components/download/selection.jsx
@@ -47,7 +47,12 @@ const DownloadSelection = () => {
 
   return (
     <div className="md:w-[85%] w-full mx-auto md:p-0 p-4">
-      <label htmlFor="platform" className="block text-sm font-medium text-white">Select a platform:</label>
+      <h1 className="text-off-white text-center pt-2 text-3xl drop-shadow-header-drop">
+        Download Rhino Linux
+      </h1>
+      <h2 className="text-off-white text-center text-2xl font-light mt-2 drop-shadow-header-drop">
+        Version <b><b>2024.2</b></b>
+      </h2>
       <select
         id="platform"
         value={selectedPlatform}
