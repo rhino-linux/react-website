bad1d94981c40c9f92c6d61e12a485123c5808e3
ook37 <oren+12345@taumoda.com>
2024-09-25 17:53:23 -0400

    fix nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -27,6 +27,10 @@ const baritems = [
         goto: "Download"
     },
     {
+        link: "/unicorn",
+        goto: "Unicorn"
+    },
+    {
         link: "https://blog.rhinolinux.org",
         goto: "Blog"
     },
@@ -43,27 +47,28 @@ const baritems = [
 function NavBar() {
     return (
         <div>
-            <nav className="w-full m-auto flex items-center justify-between">
-                <div className="p-4">
-                    <Link href="/">
-                        <Image src="/img/logo.png" alt="" width="200" height="200" className="w-[5vw]" />
+            <nav className="w-full m-auto flex items-center md:justify-center justify-between">
+                <Link href="/" className="md:hidden p-4 hover:brightness-50 hover:opacity-80 transition-all">
+                    <Image src="/img/logo.png" alt="" width="200" height="200" className="m-2 mt-4 w-[10vw]" />
+                </Link>
+                <div className="p-2 px-4 text-center m-4 bg-site-300/30 backdrop-blur shadow-black/40 shadow-lg text-xl bg-opacity-35 rounded-[0.65em] md:flex lg:gap-8 md:gap-8 hidden md:block">
+                    <Link href="/" className="hover:scale-110 my-auto hover:brightness-50 hover:opacity-80 transition-all">
+                        <Image src="/img/logo.png" alt="" width="50" height="50" />
                     </Link>
-                </div>
-                <div className="p-2 px-4 text-center m-4 rounded-lg md:flex lg:gap-8 md:gap-4 hidden md:block">
-                        {baritems.map((item) => (
-                            <p key={item.link}><Link href={item.link} className="text-xl text-white hover:text-rhino-purple transition-all">{item.goto}</Link></p>
-                        ))}
+                    {baritems.map((item) => (
+                        <p key={item.link} className="my-auto hover:scale-105 transition-all"><Link href={item.link} className="text-xl text-white hover:text-rhino-purple transition-all">{item.goto}</Link></p>
+                    ))}
                 </div>
                 <div
                     className="p-2 px-4 cursor-pointer md:hidden"
                     onClick={() => toggleMegaMenu('mega-menu')}
                 >
-                    <h1 className="text-4xl text-white">☰</h1>
+                    <h1 className="text-[10vw] hover:text-rhino-purple transition-all text-white">☰</h1>
                 </div>
             </nav>
             <div className="hidden transition-all w-full p-2" id="mega-menu">
                 <div className="w-full m-auto">
-                    <div className="md:hidden bg-site-300/30 backdrop-blur shadow-black/40 shadow-lg text-xl bg-opacity-35 rounded-[0.65em] p-2 m-4">
+                    <div className="md:hidden bg-site-300/30 backdrop-blur shadow-black/40 shadow-lg text-2xl bg-opacity-35 rounded-[0.65em] p-2 m-4">
                         {baritems.map((item) => (
                             <li className="list-none p-4" key={item.link}>
                                 <Link href={item.link}>
